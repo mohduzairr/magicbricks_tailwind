c6b5bfcedbee5541f11555a6003c829698729206
uzairr <mohduzair1990@gmail.com>
2022-01-15 20:07:09 +0530

    header

--- src/common/component/Header/index.js
+++ src/common/component/Header/index.js
@@ -2,16 +2,19 @@ import React, { useState } from 'react'
 
 
 export const Header = () => {
-    const [hide, setHide] = useState(false)
+    const [ctyhide, setCtyhide] = useState(false)   
+    const [lgnhide, setLgnhide] = useState(false)
+
+
     return (
         <>
         <div className="lg:w-full h-12 bg-red flex justify-between lg:items-center sm:w-full md:w-full sm:items-center">
             <div className="flex lg:ml-36 pl-12  md:ml-10">
                 <div className="text-2xl text-white font-bold"><span>Propnex</span></div>
-                <div id="city_effect" onMouseEnter={()=>setHide(true)} className="text-lg text-white ml-12 pt-1 font-light"> <span >City</span> <i id="down" class="fa fa-caret-down text-white"></i>  <i id="up" class="fa fa-caret-up text-white hidden"></i></div>
+                <div onMouseEnter={()=>setCtyhide(true)} onMouseLeave={()=>setCtyhide(false)} className="text-lg text-white ml-12 pt-1 font-light"> <span >City</span> { ctyhide==false ? <i  class="fa fa-caret-down text-white"></i> : <i  class="fa fa-caret-up text-white "></i> }</div>
             </div>
             <div className="flex lg:mr-28 mr-5 pr-12  ">
-                <div id="login_effect" className="lg:mr-8 mr-4 pt-2 text-white font-light"> <span>Login</span> <i id="l_down" class="fa fa-caret-down text-white"></i> <i id="l_up" class="fa fa-caret-up text-white hidden"></i></div>
+                <div  onMouseEnter={()=>setLgnhide(true)} onMouseLeave={()=>setLgnhide(false)} className="lg:mr-8 mr-4 pt-2 text-white font-light"> <span>Login</span> { lgnhide==false ? <i  class="fa fa-caret-down text-white"></i> : <i  class="fa fa-caret-up text-white "></i>}</div>
                 <div className="h-8 w-44 bg-white rounded-full pl-3 lg:pt-1 ring-offset-2 ring-2 ring-yellow"><span>Post Property </span> <span className="text-xs ml-3 font-medium h-3 w-5  bg-yellow rounded-full p-1 mt-2">FREE</span></div>
             </div>
 
