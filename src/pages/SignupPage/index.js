4dfb2cc32c05f4f1e17f5b2d1de8c3b2406896f6
uzairr <mohduzair1990@gmail.com>
2022-02-01 20:25:53 +0530

    loginpage

--- src/pages/SignupPage/index.js
+++ src/pages/SignupPage/index.js
@@ -21,40 +21,40 @@ const Signup = () => {
               <label>I am</label>
             </div>
 
-            <div className=" mt-2">
+            <div className=" mt-2 ">
               <input type="radio" name="flexRadioDefault" value="1"/>
-                <label class="p-radios" for="name">Buyer/Owner </label>
-               <input type="radio" name="flexRadioDefault" value="2"/>
-                 <label class="p-radios" for="name"> Agent</label>
-               <input type="radio" name="flexRadioDefault" value="3"/>
-                 <label class="p-radios" for="name">Builder </label>
+                <label class="p-radios" for="name" className="pl-1">Buyer/Owner </label>
+               <input type="radio" name="flexRadioDefault" value="2" className="ml-6"/>
+                 <label class="p-radios" for="name" className="pl-1"> Agent</label>
+               <input type="radio" name="flexRadioDefault" value="3" className="ml-6"/>
+                 <label class="p-radios" for="name" className="pl-1">Builder </label>
              </div>
 
              <div className="mt-4 " >
                 <div className="mt-6 ">
-                 <input type="text"  placeholder="Name" id="name" value=""/>
+                 <input type="text"  placeholder="Name"  className="text-xl font-light border-b-2 border-b-bordergrey outline-none text-lightgrey"/>
                 </div>
               <div className="mt-6">
-                   <input type="text"  placeholder="Email" id="email" value=""/>
+                   <input type="text"  placeholder="Email"  className="text-xl font-light border-b-2 border-b-bordergrey outline-none text-lightgrey"/>
                </div>
                <div className="mt-6">
-                    <input type="text"  placeholder="Username" id="username" value=""/>
+                    <input type="text"  placeholder="Username"  className="text-xl font-light border-b-2 border-b-bordergrey outline-none text-lightgrey"/>
                 </div>
                  <div className="mt-6" >
-                   <input type="password"  placeholder="Password" id="password" value=""/>
+                   <input type="password"  placeholder="Password"  className="text-xl font-light border-b-2 border-b-bordergrey outline-none text-lightgrey"/>
                  </div>
                  <div className="mt-6">
-                     <input type="text" name="phone" placeholder="Mobile Number"   value=""/>
+                     <input type="text" name="phone" placeholder="Mobile Number" className="text-xl font-light border-b-2 border-b-bordergrey outline-none text-lightgrey"/>
                  </div>
                 
-                 <div className="mt-6"><label><input type="checkbox" checked=""/> I agree to PropNex T&amp;C, Privacy Policy, &amp; Cookie Policy</label><br/><br/></div>
+                 <div className="mt-6"><label className="text-xs"><input type="checkbox"  /> I agree to PropNex T&amp;C, Privacy Policy, &amp; Cookie Policy</label><br/><br/></div>
 
              </div>
               <div className="h-10 w-72 bg-onclick text-white rounded-full ml-4 text-center text-xl pt-1">
                  <span>Sign Up</span>
               </div>
                  
-                 <div className="ml-6 pl-6 mt-4">Already registered? <a class="p-loginbtn1" href="#">Login Now</a></div>
+                 <div className="ml-6 pl-6 mt-4 ">Already registered? <span class="p-loginbtn1"  className="text-red">Login Now</span></div>
             </div>
           </div>
           </div>
